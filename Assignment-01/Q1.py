c735584d3689cca031162c29dc444b331da40666
Sid Latkar <siddhalatkar@gmail.com>
2025-12-11 22:19:05 +0530

    Assignment 01

--- Assignment-01/Q1.py
+++ Assignment-01/Q1.py
@@ -1,1 +1,22 @@
+# Take input from user
+s = input("Enter a sentence: ")
 
+# Number of characters
+NO_Chars = len(s)
+
+# Number of words
+words = s.split()
+NO_words = len(words)
+
+# Number of vowels
+vowels = "aeiouAEIOU"
+num_vowels = 0
+
+for char in s:
+    if char in vowels:
+        num_vowels += 1
+
+# Print results
+print("Number of characters:", NO_Chars)
+print("Number of words:", NO_words)
+print("Number of vowels:", num_vowels)
