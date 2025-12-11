e1241f88a595e8c454ad9a32f5195a108c8c8ee3
Sid Latkar <siddhalatkar@gmail.com>
2025-12-11 22:19:05 +0530

    Assignment 01

--- Assignment-01/Q2.py
+++ Assignment-01/Q2.py
@@ -1,1 +1,21 @@
 
+numbers = input("Enter numbers separated by commas: ")
+
+
+num_list = [int(x.strip()) for x in numbers.split(",")]
+
+
+even_count = 0
+odd_count = 0
+
+
+for num in num_list:
+    if num % 2 == 0:
+        even_count += 1
+    else:
+        odd_count += 1
+
+
+
+print("Even numbers count:", even_count)
+print("Odd numbers count:", odd_count)
