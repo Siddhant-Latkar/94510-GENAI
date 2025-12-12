95f032c02df312b22d94b7c346a5eeba9a709dae
Sid Latkar <siddhalatkar@gmail.com>
2025-12-12 10:19:47 +0530

    area calculation

---
+++ Assignment-02/Q1/area_calculation.py
@@ -0,0 +1,15 @@
+import math
+
+def circle(radious):
+    print("Area of circle is= ",math.pi*radious*radious)
+
+
+def squre(leangth):
+    print(" Area of Squre is=",leangth*leangth)
+
+
+def rectangle(leangth,width):
+    print("Area of Rectangle",leangth*width)
+
+def triangle(base,height):
+    print("Area of Triangle=",0.5*base*height)
--- Assignment-02/Q1/main.py
+++ Assignment-02/Q1/main.py
@@ -1,1 +1,36 @@
 
+import area_calculation
+
+while (True):
+
+        print("\n1.Area of circle")
+        print("2.Area of squre")
+        print("3.Area of rectangle")
+        print("4.Area of triangle")
+        print("5.EXIT")
+
+        choice=int(input("\nEnter choice="))
+        if(choice==1):
+            r=float(input("Enter radious of circle="))
+            area_calculation.circle(r)
+        elif(choice==2):
+            l=float(input("\n Enter leangth of squre="))
+            area_calculation.squre(l)
+
+        elif(choice==3):
+            l=float(input("Enter leangth of rectangle="))
+            w=float(input("Enter Width of rectangle="))
+            area_calculation.rectangle(l,w)
+
+        elif(choice==4):
+            b=float(input("Enter base of Triangle="))
+            h=float(input("Enter height of Triangle="))
+            area_calculation.triangle(b,h)
+        elif (choice ==5):
+            print("Exiting... Goodbye!")
+            break
+        
+        else:
+            print("Invalid Choice! Please try again.")
+
+
