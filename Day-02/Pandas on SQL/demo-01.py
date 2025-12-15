29805129432cd93018e5fbba1e69953d9fea5351
Sid Latkar <siddhalatkar@gmail.com>
2025-12-15 09:48:04 +0530

    pandas on sql

--- Day-02/Pandas on SQL/demo-01.py
+++ Day-02/Pandas on SQL/demo-01.py
@@ -1,1 +1,10 @@
 
+import pandas as pd
+import pandasql as ps
+
+emp_hdr =pd.read_csv("emp_hdr.csv")
+query= """ SELECT empno from emp_hdr"""
+
+result =ps.sqldf(query,{"emp_hdr": emp_hdr})
+print(result)
+
